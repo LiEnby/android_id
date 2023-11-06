f0ac4238ffe0258bd62ab30e85ff2d333f955f61
Li <li@silica.codes>
2023-11-06 13:25:23 +1300

    add psm uid

--- app/src/main/java/pink/yuv/android_id/MainActivity.java
+++ app/src/main/java/pink/yuv/android_id/MainActivity.java
@@ -1,11 +1,11 @@
 package pink.yuv.android_id;
 
 import android.app.Activity;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
-import android.os.PersistableBundle;
-import android.provider.Settings;
+import android.provider.Settings.Secure;
 import android.widget.TextView;
-
 import org.jetbrains.annotations.Nullable;
 
 public class MainActivity extends Activity {
@@ -14,9 +14,22 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        TextView androidIdTextView = (TextView)findViewById(R.id.textView);
-        String android_id = Settings.Secure.getString(getContentResolver(), Settings.Secure.ANDROID_ID);
+        TextView androidIdTextView = (TextView)findViewById(R.id.androidId);
+        String android_id = Secure.getString(getContentResolver(), Secure.ANDROID_ID);
         System.out.println("android_id: "+android_id);
         androidIdTextView.setText("android_id: "+android_id);
+
+        String psmAppId = "app not installed";
+
+        try{
+            ApplicationInfo psmAppInfo = this.getApplicationContext().getPackageManager().getApplicationInfo("com.playstation.psstore", 0);
+            if(psmAppInfo != null)
+                psmAppId = String.valueOf(psmAppInfo.uid);
+        }
+        catch (PackageManager.NameNotFoundException e) { };
+
+        TextView psmAppIdTextView = (TextView)findViewById(R.id.psmAppId);
+        System.out.println("psstore_uid: "+psmAppId);
+        psmAppIdTextView.setText("psstore_uid: "+psmAppId);
     }
 }
